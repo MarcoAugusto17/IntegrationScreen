bd69186bf52511afaa0cf8c6a64e7a7a8d68a2cf
marco742004@gmail.com <marco742004@gmail.com>
2022-12-26 08:12:08 -0300

    Comitado

---
+++ app/src/main/java/com/example/integrationscreen/CardInfo.kt
@@ -0,0 +1,6 @@
+package com.example.integrationscreen
+
+data class CardInfo(
+    val title: String,
+    val body: String
+)
--- app/src/main/java/com/example/integrationscreen/MainActivity.kt
+++ app/src/main/java/com/example/integrationscreen/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -18,9 +20,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 
 
@@ -47,17 +59,17 @@ fun MyApp() {
 
 @Composable
 fun Greeting(
-    names: List<String> = listOf(
-        "Recebido 1",
-        "Recebido 2",
-        "Recebido 3",
-        "Recebido 4",
-        "Recebido 5",
-        "Recebido 6",
-        "Recebido 7",
-        "Recebido 8",
-        "Recebido 9",
-        "Recebido 10"
+    datas: List<String> = listOf(
+        listOf(
+        CardInfo(
+            "Anotações",
+            "Comprar carro"
+        ),
+        CardInfo(
+            "Anotações",
+            "Casa"
+        )
+    ).toString()
     )
 ) {
     Surface(color = MaterialTheme.colors.background) {
@@ -66,8 +78,8 @@ fun Greeting(
                 .padding(vertical = 4.dp)
                 .verticalScroll(rememberScrollState())
         ) {
-            for (name in names) {
-                Greeting(name)
+            for (data in datas) {
+                Greeting(data)
             }
         }
     }
@@ -84,8 +96,10 @@ fun Greeting(name: String) {
         )
     )
     Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+        color = Color(0xFFAAF3ED),
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+            .clip(RoundedCornerShape(percent = 10))
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
@@ -93,15 +107,26 @@ fun Greeting(name: String) {
                     .weight(1f)
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
-                Text(text = "Titulo", style = MaterialTheme.typography.h6)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h6,
+                    color = Color.Black,
+                    fontWeight = FontWeight.ExtraBold
+                )
                 Text(
                     text = name,
                     style = MaterialTheme.typography.body2,
-                    fontWeight = FontWeight.ExtraBold
                 )
             }
-            OutlinedButton(onClick = { expanded = !expanded }) {
-                Text(if (expanded) "Show less" else "Show more")
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+                )
             }
         }
     }
@@ -115,10 +140,13 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Welcome to the Basics Codelab!")
+            Text(
+                "Welcome to the Basics Codelab!",
+                modifier = Modifier.padding(vertical = 24.dp)
+            )
             Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onContinueClicked
+                onClick = onContinueClicked,
+                modifier = Modifier.clip(RoundedCornerShape(percent = 30))
             ) {
                 Text("Continue")
             }
